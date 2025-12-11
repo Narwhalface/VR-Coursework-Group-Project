2eaacd2cb38499b7006a97ae98486e87031351fd
simplysem <109747678+simplysem@users.noreply.github.com>
2025-12-12 00:57:33 +0100

    flashlight sounds, tutorial and clipping cam fix

--- Assets/Scripts/FlashlightControl.cs
+++ Assets/Scripts/FlashlightControl.cs
@@ -11,6 +11,7 @@ public class FlashlightControl : MonoBehaviour
     private InputAction chargeLight;
 
     private int flickerAmount = 0;
+    private int chargeDelay = 0;
 
     private void Awake()
     {
@@ -31,22 +32,35 @@ public class FlashlightControl : MonoBehaviour
         if (flickerAmount > 5 && charge > 0f)
         {
             flicker = Random.Range(-0.8f, 0.8f);
+
             flickerAmount = 0;
         }
-        
-        if (chargeLight.WasPressedThisFrame())
+
+        if (0.1f < charge && charge < 3f && !flashlight.GetComponents<AudioSource>()[1].isPlaying)
         {
+            flashlight.GetComponents<AudioSource>()[1].Play();
+        } else if (charge <= 0.1f)
+        {
+            flashlight.GetComponents<AudioSource>()[1].Stop();
+        }
+
+        if (chargeLight.WasPressedThisFrame() && chargeDelay <= 0)
+        {
+            chargeDelay = 250;
+            flashlight.GetComponents<AudioSource>()[1].Stop();
+            flashlight.GetComponents<AudioSource>()[0].Play();
             prompt.SetActive(false);
             charge += Time.deltaTime * 50f;
-            charge = Mathf.Clamp(charge, 0f, 5);
-            flashlight.GetComponent<Light>().intensity = Mathf.Clamp(charge + flicker, 0f, 2f);
+            charge = Mathf.Clamp(charge, 0f, 6);
+            flashlight.GetComponent<Light>().intensity = Mathf.Clamp(charge + flicker, 0f, 3f)*1.5f;
         }
         else
         {
             charge = charge - Time.deltaTime * 0.2f;
-            charge = Mathf.Clamp(charge, 0f, 5);
-            flashlight.GetComponent<Light>().intensity = Mathf.Clamp(charge + flicker, 0f, 2f);
+            charge = Mathf.Clamp(charge, 0f, 6);
+            flashlight.GetComponent<Light>().intensity = Mathf.Clamp(charge + flicker, 0f, 3f)*1.5f;
         }
         flickerAmount += 1;
+        chargeDelay -= 1;
     }
 }
